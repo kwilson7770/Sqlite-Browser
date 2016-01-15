1aad78c66f600157b0e64d5ad76c4b2f5374a039
kwilson7770 <kwilson7770@users.noreply.github.com>
2016-01-15 06:58:35 -0600

    Finally have a functioning database reader

---
+++ Sqlite Browser/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ComboBoxTables = New System.Windows.Forms.ComboBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextBoxDBPath = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonLoadDB = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 64)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(688, 366)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'ComboBoxTables
+        '
+        Me.ComboBoxTables.FormattingEnabled = True
+        Me.ComboBoxTables.Location = New System.Drawing.Point(450, 36)
+        Me.ComboBoxTables.Name = "ComboBoxTables"
+        Me.ComboBoxTables.Size = New System.Drawing.Size(126, 21)
+        Me.ComboBoxTables.TabIndex = 4
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.ButtonLoadDB)
+        Me.Panel1.Controls.Add(Me.TextBoxDBPath)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.ComboBoxTables)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(688, 64)
+        Me.Panel1.TabIndex = 5
+        '
+        'TextBoxDBPath
+        '
+        Me.TextBoxDBPath.Location = New System.Drawing.Point(12, 38)
+        Me.TextBoxDBPath.Name = "TextBoxDBPath"
+        Me.TextBoxDBPath.Size = New System.Drawing.Size(335, 20)
+        Me.TextBoxDBPath.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Database Path"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(447, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Tables"
+        '
+        'ButtonLoadDB
+        '
+        Me.ButtonLoadDB.Location = New System.Drawing.Point(353, 36)
+        Me.ButtonLoadDB.Name = "ButtonLoadDB"
+        Me.ButtonLoadDB.Size = New System.Drawing.Size(91, 23)
+        Me.ButtonLoadDB.TabIndex = 8
+        Me.ButtonLoadDB.Text = "Load Database"
+        Me.ButtonLoadDB.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(688, 430)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ComboBoxTables As ComboBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TextBoxDBPath As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonLoadDB As Button
+End Class
---
+++ Sqlite Browser/Form1.vb
@@ -0,0 +1,210 @@
+﻿Imports System.Data.SQLite
+'Make a refrence to C:\Program Files\System.Data.SQLite\2010\bin\System.Data.SQLite.dll
+'this is 64bit only due to sqlite being 64bit dll
+
+Public Class Form1
+    Enum DBCol
+        TheDate
+        Amount
+        Store
+        Category
+        Comment
+    End Enum
+
+    'Some hard coded stuff for testing
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            'Sets up the DataGridView
+            SetupDGView()
+
+            'Some hard coded stuff for testing
+            TextBoxDBPath.Text = "G:\AE\desktop\Database.db3"
+        Catch ex As Exception
+            MsgBox(ex.ToString, vbCritical, "Error Loading")
+        End Try
+    End Sub
+
+    Private Sub SetupDGView()
+        'some properties to setup
+        DataGridView1.AllowUserToAddRows = True
+        DataGridView1.AllowUserToDeleteRows = True
+        DataGridView1.AllowUserToOrderColumns = False
+        DataGridView1.AllowUserToResizeColumns = True
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+    End Sub
+
+    Private Sub CreateTable(DataBasePath As String, TableName As String)
+        Dim MyConnection As New SQLite.SQLiteConnection
+        Dim MyCommand As SQLiteCommand
+        'This will created the database and setup up the table and columns!
+        MyConnection.ConnectionString = "Data Source=" & DataBasePath & ";"
+        MyConnection.Open()
+        MyCommand = MyConnection.CreateCommand
+        'SQL query to Create Table
+        MyCommand.CommandText = "CREATE TABLE " & TableName & " (id INTEGER PRIMARY KEY AUTOINCREMENT, col" & DBCol.TheDate & " TEXT, col" & DBCol.Amount & " FLOAT, col" & DBCol.Store & " TEXT, col" & DBCol.Category & " TEXT, col" & DBCol.Comment & " TEXT);"
+        MyCommand.ExecuteNonQuery()
+        MyCommand.Dispose()
+        MyConnection.Close()
+    End Sub
+
+    Private Function GetColumnsInTable(DataBasePath As String, TableName As String) As String()
+        Dim DataTable = New DataTable, ToReturn() As String = Nothing
+
+        Using MyConnection As New SQLite.SQLiteConnection("Data Source=" & DataBasePath & ";")
+            Using MyCommand As New SQLiteCommand("PRAGMA table_info('" & TableName & "');", MyConnection)
+                Using SQLDataAdapter As New SQLite.SQLiteDataAdapter
+                    SQLDataAdapter.SelectCommand = MyCommand
+                    MyConnection.Open()
+                    SQLDataAdapter.Fill(DataTable)
+                    MyConnection.Close()
+                    ReDim ToReturn(DataTable.Rows.Count - 1)
+                    For i = 0 To DataTable.Rows.Count - 1
+                        '  MsgBox(DataTable.Rows(i)("Name"))
+                        ToReturn(i) = DataTable.Rows(i)("Name").ToString
+                    Next
+                End Using
+            End Using
+        End Using
+
+        Return ToReturn
+    End Function
+
+    Private Function GetTablesInDataBase(DataBasePath As String)
+        Dim ToReturn As Object = Nothing
+        Using MyConnection As New SQLite.SQLiteConnection("Data Source=" & DataBasePath & ";")
+            MyConnection.Open()
+
+            Dim Schema = MyConnection.GetSchema(SQLiteMetaDataCollectionNames.Tables)
+            ReDim ToReturn(Schema.Rows.Count - 1)
+            For i = 0 To Schema.Rows.Count - 1
+                ToReturn(i) = Schema.Rows(i) !TABLE_NAME
+                ' MsgBox(Schema.Rows(i) !TABLE_NAME)
+                'If Not Schema.Rows(i) !TABLE_NAME = "sqlite_sequence" Then ComboBoxTable.Items.Add(Schema.Rows(i) !TABLE_NAME)
+            Next
+
+            MyConnection.Close()
+        End Using
+        Return ToReturn
+    End Function
+
+    Private Function ReadDataBase(DataBasePath As String, TableName As String) As ArrayList
+        Dim ToReturn As New ArrayList
+        Using MyConnection As New SQLite.SQLiteConnection("Data Source=" & DataBasePath & ";")
+            Using MyCommand As New SQLiteCommand("SELECT * FROM " & TableName, MyConnection) 'WHERE id = '0'")
+                MyConnection.Open()
+                Using SQLreader As SQLiteDataReader = MyCommand.ExecuteReader()
+                    While SQLreader.Read()
+                        Dim Cols = GetColumnsInTable(DataBasePath, TableName)
+                        Dim TempArray(Cols.Length - 1) As String
+                        For i = 0 To Cols.Length - 1
+                            Dim Read = SQLreader(Cols(i))
+                            If Read.Equals(DBNull.Value) Then
+                                TempArray(i) = ""
+                            Else
+                                TempArray(i) = Read
+                            End If
+                        Next
+                        ToReturn.Add(TempArray)
+                    End While
+                End Using
+            End Using
+            MyConnection.Close()
+        End Using
+        Return ToReturn
+    End Function
+
+    Private Sub UpdateField(DataBasePath As String, ID As Integer, TheDate As String, Amount As Double, Store As String, Category As String, Optional Comment As String = "")
+        Dim MyConnection As New SQLite.SQLiteConnection()
+        Dim MyCommand As SQLiteCommand
+        MyConnection.ConnectionString = "Data Source=" & DataBasePath & ";"
+        MyConnection.Open()
+        MyCommand = MyConnection.CreateCommand
+        ''MyCommand.CommandText = "UPDATE " & ComboBoxTable.SelectedItem & " SET (col" & DBCol.TheDate & ", col" & DBCol.Amount & ", col" & DBCol.Store & ", col" & DBCol.Category & ", col" & DBCol.Comment & ") VALUES (@thedate, @amount, @store, @category, @comment) WHERE id = (@id)"
+        MyCommand.Parameters.Add("@id", DbType.Int32).Value = ID
+        MyCommand.Parameters.Add("@thedate", DbType.String).Value = TheDate
+        MyCommand.Parameters.Add("@amount", DbType.Double).Value = Amount
+        MyCommand.Parameters.Add("@store", DbType.String).Value = Store
+        MyCommand.Parameters.Add("@category", DbType.String).Value = Category
+        MyCommand.Parameters.Add("@comment", DbType.String).Value = Comment
+        MyCommand.ExecuteNonQuery()
+        MyCommand.Dispose()
+        MyConnection.Close()
+    End Sub
+
+    Private Function InsertField(DataBasePath As String, TheDate As String, Amount As Double, Store As String, Category As String, Optional Comment As String = "")
+        Dim NewID As Integer = -1
+        Dim MyConnection As New SQLite.SQLiteConnection()
+        Dim MyCommand As SQLiteCommand
+        MyConnection.ConnectionString = "Data Source=" & DataBasePath & ";"
+        MyConnection.Open()
+        MyCommand = MyConnection.CreateCommand
+        ''MyCommand.CommandText = "INSERT INTO " & ComboBoxTable.SelectedItem & "(col" & DBCol.TheDate & ", col" & DBCol.Amount & ", col" & DBCol.Store & ", col" & DBCol.Category & ", col" & DBCol.Comment & ") VALUES (@thedate, @amount, @store, @category, @comment)"
+        'Sets the meaning of the parameters.
+        '(@thedate, @amount, @store, @category, @comment)
+        MyCommand.Parameters.Add("@thedate", DbType.String).Value = TheDate
+        MyCommand.Parameters.Add("@amount", DbType.Double).Value = Amount
+        MyCommand.Parameters.Add("@store", DbType.String).Value = Store
+        MyCommand.Parameters.Add("@category", DbType.String).Value = Category
+        MyCommand.Parameters.Add("@comment", DbType.String).Value = Comment
+        'Runs Query
+        MyCommand.ExecuteNonQuery()
+
+        ''MyCommand.CommandText = "SELECT LAST_INSERT_ROWID() FROM " & ComboBoxTable.SelectedItem
+        MyCommand.Parameters.Clear()
+        Dim LastRowId As Integer = MyCommand.ExecuteScalar() 'only finds 1 cell = Scalar
+        ''MyCommand.CommandText = "SELECT * FROM " & ComboBoxTable.SelectedItem & " WHERE id=(@lastrowid)"
+        MyCommand.Parameters.Add("@lastrowid", DbType.Int32).Value = LastRowId
+        NewID = MyCommand.ExecuteScalar() 'only finds 1 cell = Scalar
+
+        MyCommand.Dispose()
+        MyConnection.Close()
+        Return NewID
+    End Function
+
+    Private Sub DeleteField(DataBasePath As String, ID As Integer)
+        Dim MyConnection As New SQLite.SQLiteConnection()
+        Dim MyCommand As SQLiteCommand
+        MyConnection.ConnectionString = "Data Source=" & DataBasePath & ";"
+        MyConnection.Open()
+        MyCommand = MyConnection.CreateCommand
+        'SQL query to Delete Table
+        ''MyCommand.CommandText = "DELETE FROM " & ComboBoxTable.SelectedItem & "WHERE id = (@id);"
+        MyCommand.Parameters.Add("@id", DbType.Int32).Value = ID
+        MyCommand.ExecuteNonQuery()
+        MyCommand.Dispose()
+        MyConnection.Close()
+    End Sub
+
+    Private Sub ComboBoxTables_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxTables.SelectedIndexChanged
+        DataGridView1.Rows.Clear()
+        DataGridView1.Columns.Clear()
+        Dim Cols = GetColumnsInTable(TextBoxDBPath.Text, ComboBoxTables.SelectedItem)
+        If Not IsNothing(Cols) Then
+            For Each i In Cols
+                DataGridView1.Columns.Add(i, i)
+            Next
+            Dim Data = ReadDataBase(TextBoxDBPath.Text, ComboBoxTables.SelectedItem)
+            If Data.Count > 0 Then
+                For Each i As String() In Data
+                    DataGridView1.Rows.Add(i)
+                Next
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonLoadDB_Click(sender As Object, e As EventArgs) Handles ButtonLoadDB.Click
+        If IO.File.Exists(TextBoxDBPath.Text) Then
+            For Each i In GetTablesInDataBase(TextBoxDBPath.Text)
+                ComboBoxTables.Items.Add(i)
+            Next
+            If ComboBoxTables.Items.Count > 0 Then
+                ComboBoxTables.SelectedIndex = 0
+            Else
+                MsgBox("Not tables found in the database", vbInformation, "")
+            End If
+        Else
+            MsgBox("Could not find the file path specificed", vbCritical, "Error")
+        End If
+    End Sub
+End Class
